e093adf26e3926f3f6102c48142485c2d8d405d6
ArmykCZ <matejjaros129@post.cz>
2026-07-16 02:16:49 +0200

    práce na kusu utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -2,23 +2,36 @@ import os
 import time
 import json
 import datetime
+from pathlib import Path
+import sys 
+import subprocess
 
 # TODO:
 # - jednotný error handler
 # - exception management
 # - config loader
 
-def clear_terminal():
-    """
-    Vyčistí CLI
-    """
 
+
+def clear_terminal():
+    if sys.platform.startswith('win'):
+        subprocess.run('cls', shell=True)
+    else:
+        subprocess.run('clear', shell=True)
 
 def timestamp():
-    """
-    Vrátí aktuální čas
-    """
+    hour = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    print(hour)
 
+
+def create_directory():
+    #the / automatically take it as a root directory
+    #Path.cwd() gives the current working directory
+    path = (Path.cwd() / "logs").resolve() 
+    if path.exists() and path.is_dir():
+        print(path.resolve())
+    else:
+        print("neexistuje" + str(path.resolve()))
 
 def save_json(data,path):
     """
@@ -38,7 +51,11 @@ def check_dependencies():
     """
 
 
-def create_directory(path):
-    """
-    Vytvoření složek
-    """
+
+
+
+if __name__ == "__main__":
+    # Testování funkcí
+    clear_terminal()
+    timestamp()
+    create_directory()
